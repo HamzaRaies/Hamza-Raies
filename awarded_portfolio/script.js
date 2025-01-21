b7c5820e9ba94c1a119bae6193e557ad06cc8835
HamzaRaies <158738428+HamzaRaies@users.noreply.github.com>
2025-01-21 19:36:08 +0500

    Add files via upload

---
+++ awarded_portfolio/script.js
@@ -0,0 +1,59 @@
+
+ 
+document.querySelectorAll(".reveal")
+.forEach(function(elem){
+    var parent = document.createElement("span")
+    var child = document.createElement("span")
+    parent.classList.add("parent")
+    child.classList.add("child")
+    child.innerHTML=elem.innerHTML;
+    parent.appendChild(child);
+    elem.innerHTML="";
+    elem.appendChild(parent);
+
+})
+var tl=gsap.timeline();
+tl
+.from(".child span",{
+    x:100,
+    duration:2,
+    delay:0.3,
+    stagger:.2,
+    ease:Power3.easeInOut
+
+
+})
+.to(".parent .child",{
+    y:"-100%",
+    duration:1,
+    ease:Circ.inOut
+
+})
+.to(".loader",{
+    height:0,
+    duration:.5,
+    ease:Circ.inOut
+
+})
+.to(".green",{
+    height:"100%",
+    top:0,
+    duration:.5,
+    delay:-.9,
+    ease:Circ.inOut
+
+})
+.to(".green",{
+    height:"0%",
+    top:0,
+    duration:.7,
+    delay:-.5,
+    ease:Circ.inOut
+
+})
+
+.to(".web",{
+    x:-150,
+    duration:.9,
+    ease:Power3.easeInOut
+})
